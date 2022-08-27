7a1518389ef495d36f0efa0ef8df388b9234667d
formarin <irina.formagey@gmail.com>
2022-08-28 01:23:33 +0300

    ValueType_ReferenceType feat: work with types
    Созданы классы Employer и Client. наследующие класс Person.
    Опробованы изменения свойств сущностей разных типов хранения при передаче в параметры метода.

---
+++ Models/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public struct Currency
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PracticeWithTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using Models;
+
+namespace PracticeWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                FirstName = "firstName",
+                LastName = "lastName"
+            };
+
+            var usd = new Currency
+            {
+                Code = 840,
+                Name = "usd"
+            };
+
+            BadUpdateEmployeeContract(employee);
+            employee.Contract = null;
+            employee.Contract = UpdateEmployeeContract(employee.FirstName, employee.LastName);
+
+            BadUpdateCurrency(usd);
+            UpdateCurrency(ref usd);
+        }
+
+        public static void BadUpdateEmployeeContract(Employee employee)
+        {
+            employee.Contract = $"{employee.FirstName} {employee.LastName}";
+        }
+
+        public static string UpdateEmployeeContract(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        public static void BadUpdateCurrency(Currency currency)
+        {
+            currency.Name = "rup";
+        }
+
+        public static void UpdateCurrency(ref Currency currency)
+        {
+            currency.Name = "rup";
+        }
+    }
+}
