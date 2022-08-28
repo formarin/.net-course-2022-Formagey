d156218fed75d8b8b54f21caa73417cae9ed03fd
formarin <irina.formagey@gmail.com>
2022-08-28 20:02:19 +0300

    ValueType_ReferenceType feat: practice with type casting and converting
    Созданы метод рассчета зп владельца и метод получения работника из клиента.

--- PracticeWithTypes/Program.cs
+++ PracticeWithTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Models;
+using Services;
 
 namespace PracticeWithTypes
 {
@@ -24,6 +25,17 @@ namespace PracticeWithTypes
 
             BadUpdateCurrency(usd);
             UpdateCurrency(ref usd);
+
+
+            var bankService = new BankService();
+            employee.Salary = bankService.GetOwnerSalary(123456.1, 1234, 4);
+
+            var client = new Client
+            {
+                FirstName = "firstName2",
+                LastName = "lastNam2"
+            };
+            employee = bankService.GetEmployeeFromClient(client);
         }
 
         public static void BadUpdateEmployeeContract(Employee employee)
---
+++ Services/BankService.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using System;
+
+namespace Services
+{
+    public class BankService
+    {
+        public int GetOwnerSalary(double income, double expenses, int ownersCount)
+        {
+            return (int)((income - expenses) / ownersCount);
+        }
+
+        public Employee GetEmployeeFromClient(Client client)
+        {
+            return new Employee
+            {
+                FirstName = client.FirstName,
+                LastName = client.LastName
+            };
+        }
+    }
+}
