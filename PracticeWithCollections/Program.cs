4d42e770d80ef0cf8b7f5c57a96d2fa9cd648c83
formarin <irina.formagey@gmail.com>
2022-08-31 17:38:31 +0300

    feature/List_Dictionary feat: compare search in collections

--- PracticeWithCollections/Program.cs
+++ PracticeWithCollections/Program.cs
@@ -1,4 +1,8 @@
-﻿using Services;
+﻿using Models;
+using Services;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace PracticeWithCollections
 {
@@ -10,6 +14,46 @@ namespace PracticeWithCollections
             var employeeList = testDataGenerator.GetEmployeeList();
             var clientList = testDataGenerator.GetClientList();
             var clientDictionary = testDataGenerator.GetClientDictionary();
+            var sw = new Stopwatch();
+
+            var lastListClientPhone = clientList.Last().PhoneNumber;
+            var lastListClient = new Client
+            {
+                PhoneNumber = lastListClientPhone
+            };
+
+            var lastDictionaryClientPhone = clientDictionary.Last().Key;
+            var lastDictionaryClient = new Client
+            {
+                PhoneNumber = lastDictionaryClientPhone
+            };
+
+            sw.Start();
+            var foundClients1 = clientList.FirstOrDefault(client => client.PhoneNumber == lastListClientPhone);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+
+            sw.Restart();
+            var foundClients11 = clientList.FirstOrDefault(client => client == lastListClient);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+
+            sw.Restart();
+            var foundClients2 = clientDictionary.FirstOrDefault(client => client.Value.PhoneNumber == lastDictionaryClientPhone);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+
+            sw.Restart();
+            var foundClients22 = clientDictionary.FirstOrDefault(client => client.Value == lastDictionaryClient);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+
+            sw.Restart();
+            var foundClients3 = clientDictionary[lastDictionaryClientPhone];
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+
+            Console.ReadKey();
         }
     }
 }
