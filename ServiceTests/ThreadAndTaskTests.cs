bf683e00fa681eef75945536e49a6f4c125242e3
formarin <irina.formagey@gmail.com>
2022-10-12 23:32:23 +0300

    feat: add tests of BackgroundRateUpdater and ParallelCashOUt

--- ServiceTests/ThreadAndTaskTests.cs
+++ ServiceTests/ThreadAndTaskTests.cs
@@ -2,8 +2,11 @@
 using Models;
 using Services;
 using Services.Filters;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -82,6 +85,52 @@ namespace ServiceTests
             importClients.Start();
 
             Thread.Sleep(10000);
+        }
+
+        [Fact]
+        public void BackgroundRateUpdater_PositiveTest()
+        {
+            new ClientService().AddClientList(new TestDataGenerator().GetClientList(10));
+
+            var cancelTokenSource = new CancellationTokenSource();
+            var token = cancelTokenSource.Token;
+
+            var rateUpdaterService = new RateUpdaterService();
+            var task = rateUpdaterService.RateUpdater(token);
+
+            task.Start();
+            task.Wait(30000);
+
+            cancelTokenSource.Cancel();
+        }
+
+        [Fact]
+        public void ParallelCashOUt_PositiveTest()
+        {
+            var clientService = new ClientService();
+            var clientList = new TestDataGenerator().GetClientList(1000);
+            var accountList = new List<Account>();
+
+            clientService.AddClientList(clientList);
+            for (var i = 0; i < clientList.Count; i++)
+            {
+                var account = clientService.GetClient(clientList[i].Id)
+                    .AccountCollection.FirstOrDefault(x => x.CurrencyName == "USD");
+                account.Amount += 1000;
+                clientService.UpdateAccount(account);
+                accountList.Add(account);
+            }
+
+            var cashDispenserService = new CashDispenserService();
+            var tasks = new Task[accountList.Count];
+
+            for (var i = 0; i < tasks.Length; i++)
+                tasks[i] = cashDispenserService.CashOut(100, accountList[i]);
+
+            foreach (var t in tasks)
+                t.Start();
+
+            Task.WaitAll(tasks);
         }
     }
 }
---
+++ Services/CashDispenserService.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CashDispenserService
+    {
+        public Task CashOut(double amount, Account account)
+        {
+            if (account.Amount < amount)
+                throw new Exception("Недостаточно стредств");
+
+            return new Task(() =>
+            {
+                account.Amount -= amount;
+
+                var clientService = new ClientService();
+
+                Task.Delay(7000);
+
+                clientService.UpdateAccount(account);
+            });
+        }
+    }
+}
