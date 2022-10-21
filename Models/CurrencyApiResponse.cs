a73d3c4f973f81b27ffc0edc11e6433c4bd51c91
formarin <irina.formagey@gmail.com>
2022-10-21 09:35:56 +0300

    feat: add CurrencyService

---
+++ Models/CurrencyApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class CurrencyApiResponse
+    {
+        public int error;
+        public string error_message;
+        public float amount;
+    }
+}
